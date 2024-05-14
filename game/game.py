337718a1cbb99ada430071aca66098cdd9109936
DaniilAmoshiy1 <daniil13022007god@gmail.com>
2024-05-14 09:10:02 +0400

    repush:python game/main.py feat and fix: check player on borders and fixed other errors

--- game/game.py
+++ game/game.py
@@ -1,4 +1,3 @@
-import asyncio
 import sys
 import os
 
@@ -11,46 +10,37 @@ CURRENT_FOLDER = Path(__file__).parent
 FPS = 50
 SCREEN_SIZE = (1280, 720)
 STEP_PIXELS = 50
+pygame.init()
 
 
 class Game(pygame.sprite.Sprite):
     def __init__(self, video_path, image_background, image_player):
-        pygame.display.set_mode((1280, 720))
-        self.video_path = video_path
-        self.image_background = image_background
-        self.image_player = image_player
+        """
+        This is function for initialisation, here happening
+        init Sprite with library pygame, set selfS, player speed
+        and get rect from player.
+        Also, class game inherired by 'pygame.sprite.Sprite'.
+        """
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(CURRENT_FOLDER / 'My-game-pictures/Player-cat.png')  # .convert()
-        self.rect = self.image.get_rect()
+        self.video_path = video_path
+        self.image_player = image_player
+        self.image_background = image_background
+        self.rect = image_player.get_rect()
         self.y_speed = 0
         self.x_speed = 0
 
-
-        self.scene = pygame.display.get_surface().get_rect()
-        print(f'area bottom = {self.scene.bottom}')
-        self.rect.centerx = self.scene.centerx
-        self.rect.y = 100
-        self.rect.centerx = self.scene.centerx
-        self.rect.x = 100
-        self.rect.centerx = self.scene.centerx
-
-    async def update(self):
-        # Проверьте, пересекается ли персонаж с границами сцены
-        if not self.rect.colliderect(self.scene):
-            # Если персонаж выходит за границы сцены, выполните необходимые действия
-            if self.rect.bottom > self.scene.bottom or self.rect.top < self.scene.top:
-                self.y_speed *= -1
-                self.rect.y += self.y_speed
-
-            if self.rect.left < self.scene.left or self.rect.right > self.scene.right:
-                self.x_speed *= -1
-                self.rect.x += self.x_speed
-
-                if self.y_speed < 0:
-                    self.y_speed += 0.2
-
-    async def start_loading(self):
-        pygame.init()
+    def start_loading(self):
+        """
+        This is code starting loading-screen, we are using
+        for these libraries: pygame, moviepy.editor and PIL.
+        Video put to centre, loading in variable clip, after:
+        get seconds through division on 1000,
+        transforms frame in type(format) PIL,
+        transforms image to subsequence bytes,
+        create place for image with pygame,
+        and ultimately demonstrates video in positions
+        (0, 0) through blit.
+        """
         screen = pygame.display.set_mode(SCREEN_SIZE)
         pygame.display.set_caption('Воспроизведение видео')
         clock = pygame.time.Clock()
@@ -90,39 +80,37 @@ class Game(pygame.sprite.Sprite):
                 break
 
             clock.tick(FPS)
-            await asyncio.sleep(0)
 
-
-    async def start_game(self):
-        pygame.init()
+    def start_game(self):
+        """
+        In this function we are starting game, it's working
+        after update(), this includes the background image
+        and player image, we put player in screen centre.
+        We also play music here, and there is a loop that
+        watches the key press a, w, s, d and esc.
+        There is also check for borders.
+        """
         screen = pygame.display.set_mode(SCREEN_SIZE)
         pygame.display.set_caption('Игровое окно')
-
-        self.scene = screen.get_rect()
 
         image_surface = pygame.image.load(self.image_background)
         screen.blit(image_surface, (0, 0))
         pygame.display.update()
-        player_x = 0
-        player_y = 0
-        player = pygame.image.load(self.image_player)
-        player.set_colorkey((255, 255, 255))
-        screen.blit(player, (player_x, player_y))
-        pygame.display.update()
 
+        player_x = SCREEN_SIZE[0] // 2 - self.rect.width // 2
+        player_y = SCREEN_SIZE[1] // 2 - self.rect.height // 2
 
         # Start music
         pygame.mixer.init()
         music_files = [
-            os.path.join('game-music', 'Music-1.mp3'),
-            os.path.join('game-music', 'Music-2.mp3')
+            os.path.join(CURRENT_FOLDER / 'game-music/Music-1.mp3'),
+            os.path.join(CURRENT_FOLDER / 'game-music/Music-2.mp3')
         ]
-        current_music_index = 1
-        if not pygame.mixer.get_busy():
-            current_music_index = (current_music_index + 1) % len(music_files)
-            music_file = music_files[current_music_index]
-            pygame.mixer.music.load(music_file)
-            pygame.mixer.music.play()
+        current_music_index = 0
+
+        music_file = music_files[current_music_index]
+        pygame.mixer.music.load(music_file)
+        pygame.mixer.music.play()
 
         print('We are inside start_game.')
 
@@ -135,26 +123,38 @@ class Game(pygame.sprite.Sprite):
                         pygame.quit()
                         sys.exit()
                     elif event.key == pygame.K_w:
-                        player_y -= STEP_PIXELS
+                        if player_y > 0:  # проверка границы экрана сверху
+                            player_y -= STEP_PIXELS
                     elif event.key == pygame.K_s:
-                        player_y += STEP_PIXELS
+                        if player_y < SCREEN_SIZE[1] - self.rect.height:  # проверка границы экрана снизу
+                            player_y += STEP_PIXELS
                     elif event.key == pygame.K_d:
-                        player_x += STEP_PIXELS
+                        if player_x < SCREEN_SIZE[0] - self.rect.width:  # проверка границы экрана справа
+                            player_x += STEP_PIXELS
                     elif event.key == pygame.K_a:
-                        player_x -= STEP_PIXELS
-                # elif event.type == pygame.KEYUP:
-                #     if event.key in (pygame.K_w, pygame.K_s):
-                #         self.y_speed = 0
-                #     elif event.key in (pygame.K_a, pygame.K_d):
-                #         self.x_speed = 0
-            #
-            # player_x += self.x_speed
-            # player_y += self.y_speed
+                        if player_x > 0:  # проверка границы экрана слева
+                            player_x -= STEP_PIXELS
+
+            player_x = max(0, min(player_x, SCREEN_SIZE[0] - self.rect.width))
+            player_y = max(0, min(player_y, SCREEN_SIZE[1] - self.rect.height))
+
+            print(f'{SCREEN_SIZE[0] = }')
+            print(f'{SCREEN_SIZE[1] = }')
+
+            print(f'{player_x = }')
+            print(f'{player_y = }')
+
+            print(f'{SCREEN_SIZE[0] - self.rect.width = }')
+            print(f'{SCREEN_SIZE[1] - self.rect.height = }')
 
             screen.fill((0, 0, 0))
             screen.blit(image_surface, (0, 0))
-            screen.blit(player, (player_x, player_y))
+            screen.blit(self.image_player, (player_x, player_y))
             pygame.display.update()
+            if not pygame.mixer.music.get_busy():
+                current_music_index = (current_music_index + 1) % len(music_files)
+                pygame.mixer.music.load(music_files[current_music_index])
+                pygame.mixer.music.play()
 
         # Stop music
         pygame.mixer.quit()
--- game/main.py
+++ game/main.py
@@ -1,14 +1,21 @@
-import asyncio
 import os
+import pygame
+from pathlib import Path
 
 import game
+CURRENT_FOLDER = Path(__file__).parent
+
+
+def main():
+    video_path = os.path.join(CURRENT_FOLDER / "My-game-pictures/loading-line.mp4")
+    image_background = os.path.join(CURRENT_FOLDER / "My-game-pictures/roads in nature.jpg")
+    image_player = pygame.image.load(CURRENT_FOLDER / 'My-game-pictures/Player-cat.png')
+    player = game.Game(video_path, image_background, image_player)
+    image_player.set_colorkey((255, 255, 255))
+    player.start_loading()
+    player.start_game()
+    return
 
 
 if __name__ == '__main__':
-    video_path = os.path.join("My-game-pictures", "loading-line.mp4")
-    image_background = os.path.join("My-game-pictures", "roads in nature.jpg")
-    image_player = os.path.join("My-game-pictures", "Player-cat.png")
-    player = game.Game(video_path, image_background, image_player)
-    asyncio.run(player.start_loading())
-    asyncio.run(player.update())
-    asyncio.run(player.start_game())
+    main()
