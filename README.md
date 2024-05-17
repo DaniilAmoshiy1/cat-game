640c659d0611f56b0946f01226b6f9fa54811f7d
DaniilAmoshiy1 <daniil13022007god@gmail.com>
2024-05-17 21:47:14 +0400

    my project cat-game
    This is game where you can listen music and move cat

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-# PYGAME+PYGBAG template
+# This is my project "cat-game"
 
-A working template to start coding. Just a colored screen and some infrastructure.
+## Only for PC or laptop.
+
+## What needs from hardware and software for interacting with code:
+
+Keyboard, mouse, and random IDE.
+
+## A little about the project:
+
+In this game there are only three files that are responsible for the game,\
+there is no special logic in the game, first the loading screen starts,\
+and then the game background and the player (cat), you can just walk\
+in the game. There is also music here, it is possible to control it.
+
+### Controls:
+The cat walks using the W, A, S, D keys.\
+You can switch music through buttons **right** and **left**\
+You can exit the game by simply closing the window or pressing the **Esc** key.
+
+### Files:
+
+Main startup file "main.py"\
+The file "game.py" contains the game class itself\
+Also file "constants.py" for constants in project
+
 
 ## Unfortunately, my project is not accessible through the site, the server is not up. You can check how working code through git clone.
+
+## Now let's look at the command line commands:
 
 ### deploy virtual environment
 
--- game/game.py
+++ game/game.py
@@ -1,25 +1,36 @@
+import asyncio
 import sys
 import os
 
 import pygame
 from moviepy.editor import VideoFileClip
 from PIL import Image
-from pathlib import Path
 
-CURRENT_FOLDER = Path(__file__).parent
-FPS = 50
-SCREEN_SIZE = (1280, 720)
-STEP_PIXELS = 50
-pygame.init()
+from constants import (
+    SCREEN_SIZE,
+    FPS,
+    CURRENT_FOLDER,
+    STEP_PIXELS,
+    VIDEO_POSITION,
+    BACKGROUND_POSITION,
+    BLACK_FILL,
+    SLEEPING_TIME_FOR_ASYNC
+)
 
 
 class Game(pygame.sprite.Sprite):
+    """
+    This is class Game which inherired by 'pygame.sprite.Sprite'.
+    It has functions: '__init__' and 'start_game'
+    They do not have a return command, so use outside
+    the game is not recommended.
+    """
+
     def __init__(self, video_path, image_background, image_player):
         """
         This is function for initialisation, here happening
-        init Sprite with library pygame, set selfS, player speed
-        and get rect from player.
-        Also, class game inherired by 'pygame.sprite.Sprite'.
+        init Sprite through library pygame, set selfs
+        and player speed.
         """
         pygame.sprite.Sprite.__init__(self)
         self.video_path = video_path
@@ -28,133 +39,141 @@ class Game(pygame.sprite.Sprite):
         self.rect = image_player.get_rect()
         self.y_speed = 0
         self.x_speed = 0
+        self.video_finished = False
 
-    def start_loading(self):
+
+
+    async def start_game(self):
         """
-        This is code starting loading-screen, we are using
-        for these libraries: pygame, moviepy.editor and PIL.
-        Video put to centre, loading in variable clip, after:
-        get seconds through division on 1000,
-        transforms frame in type(format) PIL,
-        transforms image to subsequence bytes,
-        create place for image with pygame,
-        and ultimately demonstrates video in positions
-        (0, 0) through blit.
+        In this function we are starting game,
+        code is here contains main loop for
+        start screen loading,
+        includes the background image
+        and player image, we put player in screen centre.
+        We play music here, also loop
+        watches the key press a, w, s, d, buttons
+        left and right for switch music, esc for
+        close game and program close icon(cross).
+        There is also check for borders.
+        In this is code using only one loop,
+        because it conition pygbag for create
+        page.
+        screen - set screen size.
+        clip - take the video.
+        player_x and player_y(in start) - set player position.
+        current_music_index - this is index for play music,
+        it created because type object
+        where stored path to music 'list'.
+        music_files - variable where stored path to music.
+        running(default is True) - need for set loop
+        on or off depending on conditions.
+        video_started(False) - need for start loading before game.
         """
+        print('Start working start_game()')
         screen = pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption('Воспроизведение видео')
+        pygame.display.set_caption('Window for play')
         clock = pygame.time.Clock()
 
-        scene = screen.get_rect()
-        print(f'area bottom = {scene.bottom}')
-        self.rect.centerx = scene.centerx
-        self.rect.y = 100
-        self.rect.x = 100
-
-        # Загрузка видеофайла
         clip = VideoFileClip(self.video_path)
-
-        print('We are inside start_loading.')
-
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if not event.type == pygame.KEYUP:
-                    continue
-
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-                    pygame.quit()
-                    sys.exit()
-
-            frame = clip.get_frame(pygame.time.get_ticks() / 1000)
-            pil_image = Image.fromarray(frame)
-            image_bytes = pil_image.tobytes()
-            frame_surface = pygame.image.fromstring(image_bytes, pil_image.size, pil_image.mode)
-            screen.blit(frame_surface, (0, 0))
-
-            pygame.display.update()
-
-            if pygame.time.get_ticks() / 1000 >= clip.duration:
-                print('We are leaving start_loading')
-                break
-
-            clock.tick(FPS)
-
-    def start_game(self):
-        """
-        In this function we are starting game, it's working
-        after update(), this includes the background image
-        and player image, we put player in screen centre.
-        We also play music here, and there is a loop that
-        watches the key press a, w, s, d and esc.
-        There is also check for borders.
-        """
-        screen = pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption('Игровое окно')
-
-        image_surface = pygame.image.load(self.image_background)
-        screen.blit(image_surface, (0, 0))
-        pygame.display.update()
 
         player_x = SCREEN_SIZE[0] // 2 - self.rect.width // 2
         player_y = SCREEN_SIZE[1] // 2 - self.rect.height // 2
 
         # Start music
         pygame.mixer.init()
+        current_music_index = 0
         music_files = [
             os.path.join(CURRENT_FOLDER / 'game-music/Music-1.mp3'),
-            os.path.join(CURRENT_FOLDER / 'game-music/Music-2.mp3')
+            os.path.join(CURRENT_FOLDER / 'game-music/Music-2.mp3'),
+            os.path.join(CURRENT_FOLDER / 'game-music/Music-3.mp3'),
+            os.path.join(CURRENT_FOLDER / 'game-music/Music-4.mp3'),
+            os.path.join(CURRENT_FOLDER / 'game-music/Music-5.mp3')
         ]
-        current_music_index = 0
 
-        music_file = music_files[current_music_index]
-        pygame.mixer.music.load(music_file)
+        pygame.mixer.music.load(music_files[current_music_index])
         pygame.mixer.music.play()
 
         print('We are inside start_game.')
 
         running = True
+        video_started = False
         while running:
             for event in pygame.event.get():
+                if event.type == pygame.QUIT:  # Обработка события закрытия окна
+                    running = False
+                    pygame.quit()
+                    sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
+                    if event.key == pygame.K_ESCAPE or event.type == pygame.QUIT:
                         running = False
                         pygame.quit()
                         sys.exit()
-                    elif event.key == pygame.K_w:
-                        if player_y > 0:  # проверка границы экрана сверху
-                            player_y -= STEP_PIXELS
-                    elif event.key == pygame.K_s:
-                        if player_y < SCREEN_SIZE[1] - self.rect.height:  # проверка границы экрана снизу
-                            player_y += STEP_PIXELS
-                    elif event.key == pygame.K_d:
-                        if player_x < SCREEN_SIZE[0] - self.rect.width:  # проверка границы экрана справа
-                            player_x += STEP_PIXELS
-                    elif event.key == pygame.K_a:
-                        if player_x > 0:  # проверка границы экрана слева
-                            player_x -= STEP_PIXELS
+                    if event.key == pygame.K_RIGHT:
+                        current_music_index = (current_music_index + 1) % len(music_files)
+                        pygame.mixer.music.load(music_files[current_music_index])
+                        pygame.mixer.music.play()
+                    elif event.key == pygame.K_LEFT:
+                        current_music_index = (current_music_index - 1) % len(music_files)
+                        pygame.mixer.music.load(music_files
+                                                [current_music_index])
+                        pygame.mixer.music.play()
+            if not video_started:
+                frame = clip.get_frame(pygame.time.get_ticks() / 1000)
+                pil_image = Image.fromarray(frame)
+                image_bytes = pil_image.tobytes()
+                frame_surface = pygame.image.fromstring(image_bytes, pil_image.size, pil_image.mode)
+                screen.blit(frame_surface, VIDEO_POSITION)
+                pygame.display.update()
 
-            player_x = max(0, min(player_x, SCREEN_SIZE[0] - self.rect.width))
-            player_y = max(0, min(player_y, SCREEN_SIZE[1] - self.rect.height))
+                if pygame.time.get_ticks() / 1000 >= clip.duration:
+                    video_started = True
 
-            print(f'{SCREEN_SIZE[0] = }')
-            print(f'{SCREEN_SIZE[1] = }')
+            else:
+                image_surface = pygame.image.load(self.image_background)
+                screen.blit(image_surface, BACKGROUND_POSITION)
 
-            print(f'{player_x = }')
-            print(f'{player_y = }')
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_w]:
+                    if player_y > 0:
+                        player_y -= STEP_PIXELS
+                if keys[pygame.K_s]:
+                    if player_y < SCREEN_SIZE[1] - self.rect.height:
+                        player_y += STEP_PIXELS
+                if keys[pygame.K_d]:
+                    if player_x < SCREEN_SIZE[0] - self.rect.width:
+                        player_x += STEP_PIXELS
+                if keys[pygame.K_a]:
+                    if player_x > 0:
+                        player_x -= STEP_PIXELS
+                    # Switch music:
 
-            print(f'{SCREEN_SIZE[0] - self.rect.width = }')
-            print(f'{SCREEN_SIZE[1] - self.rect.height = }')
+                # Check borders:
+                player_x = max(0, min(player_x, SCREEN_SIZE[0] - self.rect.width))
+                player_y = max(0, min(player_y, SCREEN_SIZE[1] - self.rect.height))
 
-            screen.fill((0, 0, 0))
-            screen.blit(image_surface, (0, 0))
-            screen.blit(self.image_player, (player_x, player_y))
-            pygame.display.update()
-            if not pygame.mixer.music.get_busy():
-                current_music_index = (current_music_index + 1) % len(music_files)
-                pygame.mixer.music.load(music_files[current_music_index])
-                pygame.mixer.music.play()
+                # Check zone 'borders':
+                # print(f'{SCREEN_SIZE[0] = }')
+                # print(f'{SCREEN_SIZE[1] = }')
+                #
+                # print(f'{player_x = }')
+                # print(f'{player_y = }')
+                #
+                # print(f'{SCREEN_SIZE[0] - self.rect.width = }')
+                # print(f'{SCREEN_SIZE[1] - self.rect.height = }')
+
+                screen.fill(BLACK_FILL)
+                screen.blit(image_surface, BACKGROUND_POSITION)
+                screen.blit(self.image_player, (player_x, player_y))
+                pygame.display.update()
+
+                if not pygame.mixer.music.get_busy():
+                    current_music_index = (current_music_index + 1) % len(music_files)
+                    pygame.mixer.music.load(music_files[current_music_index])
+                    pygame.mixer.music.play()
+
+            clock.tick(FPS)
+            await asyncio.sleep(SLEEPING_TIME_FOR_ASYNC)
 
         # Stop music
         pygame.mixer.quit()
+        print('Work start_game() completed')
